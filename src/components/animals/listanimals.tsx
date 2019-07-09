2f0cc29a8550f35409130735307855db815af5c2
Karega K. Scott <kkscott@outlook.com>
2019-07-08 19:18:26 -0500

    React component lifecycle events

--- src/components/animals/listanimals.tsx
+++ src/components/animals/listanimals.tsx
@@ -5,11 +5,26 @@ import {Link, Redirect} from 'react-router-dom';
 import {Button, Container, Grid, Image} from 'semantic-ui-react';
 
 class ListAnimals extends React.Component {
+    state = { documents: [], pages: 1, pageIndex: 1};
+
     constructor(props) {
         super(props);
         this.onSponsorClick = this.onSponsorClick.bind(this);
     }
-    
+
+    componentDidMount() {
+        fetch("http://localhost:3302/api/animals?limit=10")
+            .then(response =>response.json())
+            .then(response => this.setState(response))
+            .catch(error => console.log(error));
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return (nextProps !== this.props) || 
+            (this.state !== nextState) || 
+            (this.context !== nextContext);
+    }
+        
     onSponsorClick(event, data) {
         // data is both {} and a key:value collection
         // For example:
@@ -23,89 +38,21 @@ class ListAnimals extends React.Component {
     buttonAs() {
         return(<Link to="/animals/new" >New</Link>);
     }
+
     render() {
-        return <Container>
-            <h2>Animal Rescurers! We need YOU!</h2>
-            
-            <Grid stretched container>
-                <Grid.Row columns="3" stretched>
-                    <Grid.Column >
-                        <Image size="massive" src="../assets/images/polar-bear-hero.jpg"/>
-                    </Grid.Column>
-                    <Grid.Column>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat nunc massa, sed volutpat elit viverra at.
-                        Maecenas tristique, ligula vel dignissim hendrerit, ipsum tellus molestie est, in bibendum massa arcu pretium orci.
-                        Integer tempor imperdiet erat eu euismod. Cras efficitur ligula nec vestibulum commodo. Ut a lacus vel felis molestie
-                        elementum. Aliquam erat volutpat. Donec pretium aliquet dapibus. Morbi auctor feugiat sagittis. Nulla nisl augue,
-                        tempor ac lacus vitae, condimentum volutpat neque. Phasellus rutrum erat nec dui accumsan laoreet. Suspendisse
-                        in venenatis risus, sed tincidunt nunc. Sed porta quis justo at placerat. Mauris eget odio quis quam imperdiet sagittis.
-                    </Grid.Column>
-                    <Grid.Column>
-                        100
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Button animalId={23} label="Sponsor" as={this.buttonAs} onClick={this.onSponsorClick} />
-                    </Grid.Column>
-                </Grid.Row>                
-                <Grid.Row columns="3" stretched>
-                    <Grid.Column >
-                        <Image size="massive" src="../assets/images/African_Elephant_7.27.2012_hero_and_circle_HI_53941.jpg"/>
-                    </Grid.Column>
-                    <Grid.Column>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat nunc massa, sed volutpat elit viverra at.
-                        Maecenas tristique, ligula vel dignissim hendrerit, ipsum tellus molestie est, in bibendum massa arcu pretium orci.
-                        Integer tempor imperdiet erat eu euismod. Cras efficitur ligula nec vestibulum commodo. Ut a lacus vel felis molestie
-                        elementum. Aliquam erat volutpat. Donec pretium aliquet dapibus. Morbi auctor feugiat sagittis. Nulla nisl augue,
-                        tempor ac lacus vitae, condimentum volutpat neque. Phasellus rutrum erat nec dui accumsan laoreet. Suspendisse
-                        in venenatis risus, sed tincidunt nunc. Sed porta quis justo at placerat. Mauris eget odio quis quam imperdiet sagittis.
-                    </Grid.Column>
-                    <Grid.Column>
-                        100
-                    </Grid.Column>
-                    <Grid.Column>
-                        Sponsor
-                    </Grid.Column>
-                </Grid.Row>                
-                <Grid.Row columns="3" stretched>
-                    <Grid.Column >
-                        <Image size="massive" src="../assets/images/Black_Rhino_8.6.2012_Hero_and_Circle_HI_48366.jpg"/>
-                    </Grid.Column>
-                    <Grid.Column>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat nunc massa, sed volutpat elit viverra at.
-                        Maecenas tristique, ligula vel dignissim hendrerit, ipsum tellus molestie est, in bibendum massa arcu pretium orci.
-                        Integer tempor imperdiet erat eu euismod. Cras efficitur ligula nec vestibulum commodo. Ut a lacus vel felis molestie
-                        elementum. Aliquam erat volutpat. Donec pretium aliquet dapibus. Morbi auctor feugiat sagittis. Nulla nisl augue,
-                        tempor ac lacus vitae, condimentum volutpat neque. Phasellus rutrum erat nec dui accumsan laoreet. Suspendisse
-                        in venenatis risus, sed tincidunt nunc. Sed porta quis justo at placerat. Mauris eget odio quis quam imperdiet sagittis.
-                    </Grid.Column>
-                    <Grid.Column>
-                        100
-                    </Grid.Column>
-                    <Grid.Column>
-                        Sponsor
-                    </Grid.Column>
-                </Grid.Row>                
-                <Grid.Row columns="3" stretched>
-                    <Grid.Column >
-                        <Image size="massive" src="../assets/images/Green_Turtle_Hero_image_260214.jpg"/>
-                    </Grid.Column>
-                    <Grid.Column>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat nunc massa, sed volutpat elit viverra at.
-                        Maecenas tristique, ligula vel dignissim hendrerit, ipsum tellus molestie est, in bibendum massa arcu pretium orci.
-                        Integer tempor imperdiet erat eu euismod. Cras efficitur ligula nec vestibulum commodo. Ut a lacus vel felis molestie
-                        elementum. Aliquam erat volutpat. Donec pretium aliquet dapibus. Morbi auctor feugiat sagittis. Nulla nisl augue,
-                        tempor ac lacus vitae, condimentum volutpat neque. Phasellus rutrum erat nec dui accumsan laoreet. Suspendisse
-                        in venenatis risus, sed tincidunt nunc. Sed porta quis justo at placerat. Mauris eget odio quis quam imperdiet sagittis.
-                    </Grid.Column>
-                    <Grid.Column>
-                        100
-                    </Grid.Column>
-                    <Grid.Column>
-                        Sponsor
-                    </Grid.Column>
-                </Grid.Row>                
-            </Grid>
-        </Container>;
+        const documentItems = this.state.documents.map((document)=>
+            <div key={document._id}>
+                <span>{document.name}</span>
+                <span>{document.description}</span>
+            </div>);
+        
+        return (
+            <Container>
+                <h2>Animal Rescurers! We need YOU!</h2>
+                
+                {documentItems}
+            </Container>
+        );
     }
 }
 
--- src/components/animals/newanimal.tsx
+++ src/components/animals/newanimal.tsx
@@ -4,43 +4,45 @@ import * as ReactDOM from "react-dom";
 import {Form, FormInput, FormTextArea, FormCheckbox, FormButton} from "semantic-ui-react";
 
 class NewAnimal extends React.Component {
-    state;
+    state = {name:'', description: '', endangered: false, imageSrc: ''};
+    message = {};
+    error = {};
+
     constructor(props) {
         super(props);
-
-        this.state = { 
-            animal: {
-               name: '', 
-               description: '', 
-               population: 0,
-               endangered: false
-            }
-        };
-
         this.onSaveNewAnimal = this.onSaveNewAnimal.bind(this);
     }
 
-    onSaveNewAnimal(e) {
-        const animal = this.state.animal;
+    componentDidMount() {
+    }
 
-        var post = new XMLHttpRequest();
-        try {
-            post.open('POST', 'http://localhost:3302/rs/new/animal');
-            post.send(animal);
-            console.log(post.status + ': ' + post.statusText);
-        } catch(err) {
-            console.log(err);
-        }
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return (this.props !== nextProps) ||
+            (this.state !== nextState) ||
+            (this.context !== nextContext);
+    }
+
+    onSaveNewAnimal(e) {
+        const animal = this.state;
+        fetch("http://localhost:3302/api/animal/new", {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json())
+        .then(response => this.message = response)
+        .catch(error => this.error = error);
     }
 
     render() {
-        let animal = this.state.animal;
+        let animal = this.state;
 
         return (
             <Form>
                 <FormInput id='name' name='name' placeholder='Animal Name' type='text' value={animal.name}/>
                 <FormTextArea id='description' name='description' placeholder='Animal Description' type='textarea' value={animal.description}/>
-                <FormInput id='population' name='population' placeholder='Animal Population' type='number' min='1' value={animal.population}/>
+                <FormInput id='imageSrc' name='imageSrc' placeholder='Animal url image' type='text' value={animal.imageSrc}/>
                 <FormCheckbox id='endangered' name='endangered' type='checkbox' checked={animal.endangered}/>
                 <FormButton id='addanimal' name='addanimal' value='Save' onClick={this.onSaveNewAnimal}/>
             </Form>
