0081d4fb22ed7df025892d62a7ae427aafac2f6d
Karega K. Scott <kkscott@outlook.com>
2019-07-10 17:12:42 -0500

    Edit new animal function code changes

--- src/components/animals/newanimal.tsx
+++ src/components/animals/newanimal.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import {Redirect} from 'react-router-dom';
 
 import {Form, FormInput, FormTextArea, FormCheckbox, FormButton} from "semantic-ui-react";
-import { stat } from "fs";
 
-class NewAnimal extends React.Component {
+
+class NewAnimal extends React.Component<any> {
     state = {
         animal: 
         {
@@ -14,24 +12,33 @@ class NewAnimal extends React.Component {
         },
         contributor: '',
         toString: function() {
-            if(this.contributor.trim().length > 0)            
-                this.animal.contributors = [this.contributor];
+            if(this.animal.contributors.indexOf(this.contributor.trim()) !== 0) 
+                this.animal.contributors.push([this.contributor]);
                 
             return JSON.stringify(this.animal);
         }
     };
 
-    message = {};
-    error = {};
-
-
+    animalId = '';
+    message = '';
+    error = '';
+    
     constructor(props) {
         super(props);
+
+        this.animalId=this.props.animalId; 
         this.onSaveNewAnimal = this.onSaveNewAnimal.bind(this);
         this.onChange = this.onChange.bind(this);
     }
 
     componentDidMount() {
+        if(this.animalId == "0") return;
+
+        // edit existing details
+        fetch(`http://localhost:3302/api/animal/${this.animalId}`)
+            .then(response =>response.json())
+            .then(response => this.setState({["animal"]: response, ["contributor"]: ''}))
+            .catch(error => console.log(error));
     }
 
     shouldComponentUpdate(nextProps, nextState, nextContext) {
@@ -40,14 +47,17 @@ class NewAnimal extends React.Component {
             (this.context !== nextContext);
     }
 
-    onSaveNewAnimal(event, data) {
+    onSaveNewAnimal(event) {
         const stateObj = this.state;
-        console.log(stateObj.toString());
 
         var form = document.querySelector("form");
         if(!form.checkValidity()) return;
 
-        fetch("http://localhost:3302/api/animal/new", {
+        var url = `http://localhost:3302/api/animal/${this.animalId}`;
+        if(this.animalId == "0")
+            url = `http://localhost:3302/api/animal/new`;
+
+        fetch(url, {
             method: 'POST',
             body: stateObj.toString(), // returns JSON.stringify 
             headers: {
@@ -55,39 +65,54 @@ class NewAnimal extends React.Component {
             }
         }).then(response => response.json())
         .then(response => {
-            this.message = response; 
-            window.location.href='/#/animals'; })
+            console.log(response);
+            this.animalId = response._id;
+            this.forceUpdate();
+
+            /*window.location.href='/#/animals';*/ })
         .catch(error => this.error = error);
         
     }
 
-    onChange(event, data) {
+    onChange(event) {
+        const target = event.target;
+        const key = target.name;
+        const value = (target.type === 'checkbox')? target.checked : target.value;
+
         let stateObj = this.state;
-
-        if (data.id === 'endangered') {
-            stateObj.animal[data.id] = (data.checked || false);
-        } else if(data.id === 'contributor') {
-            stateObj.contributor = data.value;
-        } else {
-            stateObj.animal[data.id] = data.value;
-        }  
-
+        if(key !== "contributor")
+            stateObj["animal"][key] = value;
+        else
+            stateObj[key] = value;
+        
         this.setState(stateObj);
     }
 
-    render() {
+    render() {        
         let animal = this.state.animal;
         let contributor = this.state.contributor;
 
+        const contributors = animal.contributors.map((contributor) => {
+            return (
+                <div key={contributor}  >
+                    <span>{contributor}</span>
+                </div>
+            );    
+        });
+        
         return (
-            <Form>
-                <FormInput id='name' onChange={this.onChange} name='name' required placeholder='Animal Name' type='text' value={animal.name}/>
-                <FormTextArea id='description' onChange={this.onChange} name='description' placeholder='Animal Description' type='textarea' value={animal.description}/>
-                <FormInput id='imageSrc' onChange={this.onChange} name='imageSrc' placeholder='Animal url image' type='text' value={animal.imageSrc}/>
-                <FormCheckbox id='endangered' onChange={this.onChange} name='endangered' type='checkbox' checked={animal.endangered}/>
-                <FormInput id='contributor' onChange={this.onChange} name='contributor' type='email' value={contributor}/>
-                <FormButton id='addanimal' name='addanimal' onClick={this.onSaveNewAnimal}>Save</FormButton>
-            </Form>
+            <div>
+                <Form>
+                    <FormInput id='name' onChange={this.onChange} name='name' required placeholder='Animal Name' type='text' value={animal.name}/>
+                    <FormTextArea id='description' onChange={this.onChange} name='description' placeholder='Animal Description' type='textarea' value={animal.description}/>
+                    <FormInput id='imageSrc' onChange={this.onChange} name='imageSrc' placeholder='Animal url image' type='text' value={animal.imageSrc}/>
+                    <FormCheckbox id='endangered' onChange={this.onChange} name='endangered' type='checkbox' checked={animal.endangered}/>
+                    <FormInput id='contributor' onChange={this.onChange} name='contributor' placeholder="contributor@email.com" type='email' value={contributor}/>
+                    <FormButton id='addanimal' name='addanimal' onClick={this.onSaveNewAnimal}>Save</FormButton>
+                </Form>
+
+                {contributors}
+            </div>
         );
     }
 }
