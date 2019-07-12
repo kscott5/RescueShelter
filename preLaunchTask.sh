f98e1a9c8b139f45ebecfb790a6348313e3de290
Karega K. Scott <kkscott@outlook.com>
2019-07-12 10:19:28 -0500

    Dev: ensure ports open

--- preLaunchTask.sh
+++ preLaunchTask.sh
@@ -4,6 +4,15 @@
 #
 clear
 
+if [ "$1" = "--webapi" ]; then 
+    
+    kill $(lsof -t -i :3302)
+    if [ $? != 0 ]; then echo "Rescue Shelter webapi//localhost:3302 not open"
+    else echo "Rescue Shelter webapi//localhost:3302 closed"
+    fi
+
+fi
+
 npm run clean
 npm run compile
 
@@ -11,5 +20,11 @@ sudo docker rm mongo_dev -f
 sudo docker run -d --rm -p 27017:27017 --name mongo_dev mongo
 
 if [ "$1" = "--webapp" ]; then 
+    
+    kill $(lsof -t -i :3301)
+    if [ $? != 0 ]; then echo "Rescue Shelter webapp//localhost:3301 not open"
+    else echo "Rescue Shelter webapp//localhost:3301 closed"
+    fi
+
     npm run start 
 fi
