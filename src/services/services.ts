d2a1a3969d51fcbdec573f066d6b5f3856365b47
Karega K. Scott <kkscott@outlook.com>
2019-07-16 13:40:43 -0500

    Dev: ensure modifed model returned

--- src/services/services.ts
+++ src/services/services.ts
@@ -17,6 +17,25 @@ export function createMongooseModel(modelName: string, modelSchema: mongoose.Sch
     return __connection.model(modelName, modelSchema);
 }
 
+export function createFindOneAndUpdateOptions(fields?: Object|String) {
+    // MongoDB https://mongodb.github.io/node-mongodb-native/3.2/api/Collection.html#findOneAndUpdate
+    // Mongoose https://mongoosejs.com/docs/api.html#model_Model.findOneAndUpdate
+    
+    var options = {
+        new:  true,       // returns the modified model
+        upsert: false,    // new models are not allowed
+        maxTimeMS:  1000, // 10 seconds maximum wait
+        rawResult: true,  // returns the raw result from the MongoDB driver
+        strict: false     // ensures only model schema value saved
+    };
+
+    /*Field selection. Equivalent to .select(fields).findOneAndUpdate()*/
+    if(fields) 
+        options["fields"] = fields;
+
+    return options;
+}
+
 export function createSecret(id: string) {
     crypto.createSecretKey
 }
