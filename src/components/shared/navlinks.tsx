f848a593a6a2c50668e0ee0b797833e9e9602b53
Karega K. Scott <kkscott@outlook.com>
2019-07-16 16:18:21 -0500

    Dev: active menu item

--- src/components/shared/navlinks.tsx
+++ src/components/shared/navlinks.tsx
@@ -11,25 +11,24 @@ class NavLinks extends React.Component {
     constructor(props) {
         super(props);        
         
-        this.state = {activeMenuItem: 'home'};
         this.onMenuItemClicked = this.onMenuItemClicked.bind(this);
     }   
 
-    onMenuItemClicked(e, {name}) {
-        this.state = {activeMenuItem: name};
+    onMenuItemClicked(event, data) {
+        document.querySelector(".active").classList.remove("active");
+        document.querySelector(`div#${data.id}`).classList.add("active");
     }
 
     render() {
-        const activeMenuItem = this.state.activeMenuItem;
         return (            
             <Segment inverted vertical textAlign='center' 
                 style={{ minHeight: 50, padding: '1em 0em' }}>
                 
                 <Container>                    
-                    <Menu inverted pointing secondary size='large'>
-                        <Menu.Item id='home' name='home' as='div' active={activeMenuItem ==='home'} onClick={this.onMenuItemClicked}><Link to="/">Home</Link></Menu.Item>
-                        <Menu.Item id='animals' name='animals' as='div' active={activeMenuItem ==='animals'} onClick={this.onMenuItemClicked}><Link to="/animals">Animals</Link></Menu.Item>
-                        <Menu.Item id='members' name='contributors' as='div' active={activeMenuItem ==='sponsors'} onClick={this.onMenuItemClicked}><Link to="/sponsors">Sponsors</Link></Menu.Item>
+                    <Menu id="menu" name="menu" inverted pointing secondary size='large'>
+                        <Menu.Item id='home' name='home' as='div' active data onClick={this.onMenuItemClicked}><Link to="/">Home</Link></Menu.Item>
+                        <Menu.Item id='animals' name='animals' as='div' data onClick={this.onMenuItemClicked}><Link to="/animals">Animals</Link></Menu.Item>
+                        <Menu.Item id='sponsors' name='sponsors' as='div' data onClick={this.onMenuItemClicked}><Link to="/sponsors">Sponsors</Link></Menu.Item>
                     </Menu>
                     
                 </Container>
