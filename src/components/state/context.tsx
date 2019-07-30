e49a7ec52b53cecb23bfed932a5cd4ad57a6f2b5
Karega K. Scott <kkscott@outlook.com>
2019-07-30 10:28:36 -0500

    Dev: default sponsor context

--- src/components/state/context.tsx
+++ src/components/state/context.tsx
@@ -1,4 +1,40 @@
 import * as React from "react";
-import {SponsorStateModel} from "./sponsor";
+import {SponsorStateModel, SponsorModel} from "./sponsor";
 
-export const SponsorContext = React.createContext(new SponsorStateModel());
+export const DefaultSponsorContextValue = {
+    model: new SponsorStateModel(),
+
+    onLoggedIn: (login) => {
+        (login.ok)?
+            console.log("Login is complete.") :
+            console.log("Login failed.");
+
+        this.model.hashid = login.data.hashid || '';
+        this.model.loggedIn = login.ok || false;
+        this.model.sponsor = login.data.sponsor || new SponsorModel();
+    },
+
+    onLoggedOut: (logout) => {
+        (logout.ok)? 
+            console.log("Logout is complete.") :
+            console.log("Logout failed.");
+        
+        this.model = new SponsorStateModel();
+    },
+
+    onUpdates: (updates) => {
+        (updates.ok)?
+            console.log("Updates are complete.") :
+            console.log("Updates failed.");
+        
+        console.log(updates);
+    },
+
+    onError: (error) => {
+        console.log(`${error}`);
+    }
+
+
+};
+
+export const SponsorContext = React.createContext(DefaultSponsorContextValue);
