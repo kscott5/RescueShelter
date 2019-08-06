1a59065a5306edc42b60e9a8c9dc7c54d2a46b22
Karega K. Scott <kkscott@outlook.com>
2019-08-06 15:25:57 -0500

    Dev: a11y text

--- src/components/sponsors/listsponsors.tsx
+++ src/components/sponsors/listsponsors.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom';
+import {Container} from 'semantic-ui-react';
+import {AppContext} from '../state/context';
 
-import {Container, Image} from 'semantic-ui-react';
+const a11y = {
+    lang: 'en',
+    titles: {
+        t1: ''
+    },
+    headings: {
+        h2: 'Rescure Shelter needs sponsors!'
+    },
+    buttons: {
+    },
+    links: {
+        new: 'New',
+        edit: 'Edit',
+        view: 'View'
+    },
+    forms: {
+    }
+};
 
 class ListSponsors extends React.Component {
+    static contextType = AppContext;
     state = { documents: [], pages: 1, pageIndex: 1};
 
     constructor(props) {
@@ -11,7 +31,7 @@ class ListSponsors extends React.Component {
     }
 
     componentDidMount() {
-        fetch("http://localhost:3302/api/sponsors?limit=100")
+        fetch(`http://localhost:3302/api/sponsors?limit=100&lang=${a11y.lang}`)
             .then(response =>response.json())
             .then(response => {
                 if(response.ok) 
@@ -29,19 +49,20 @@ class ListSponsors extends React.Component {
     }
         
     render() {
+        
         const documentItems = this.state.documents.map((document)=>
             <div key={document._id}>
                 <span>{document.username}</span>
                 <span>{document.useremail} </span>
                 
-                <Link to={`/sponsor/${document._id}`}>Edit</Link>        
+                <Link to={`/sponsor/${document._id}`}>{a11y.links.view}</Link>
             </div>
         );
         
         return (
             <Container>
-                <h2>Rescure Shelter needs sponsors!</h2>
-                <Link to="/sponsor" >New</Link>
+                <h2>{a11y.headings.h2}</h2>
+                <Link to="/sponsor">{a11y.links.new}</Link>
                 {documentItems}
             </Container>
         );
