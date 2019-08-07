4f6525ce9e8fa8d2e4f3abd9b6a263551d7dee03
Karega K. Scott <kkscott@outlook.com>
2019-08-07 13:59:23 -0500

    Dev:  meta tag use @ https://ogp.me social grapher

---
+++ src/components/state/opengraph.tsx
@@ -0,0 +1,12 @@
+// https://ogp.me/
+class OpenGraph {
+    description: string ='';
+    determiner: string = '';
+    locale: string = 'en_US';
+    siteName: string = 'Rescue Shelter';
+    title: string = 'Rescue Shelter: Single Page Application, SPA';
+    type: string = 'website';
+    url: string = 'http://localhost:3301';
+
+    constructor() {}
+}
