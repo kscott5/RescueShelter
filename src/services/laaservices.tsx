b6bcf03c1d3608621eba7ef2d529cd38bd599bab
Karega K. Scott <kkscott@outlook.com>
2020-07-31 13:10:17 -0500

    Restructure without comments

--- src/services/laaservices.tsx
+++ src/services/laaservices.tsx
@@ -1,32 +1,26 @@
-// NOTE: This was a one-to-one copy found @ https://react.i18next.com/latest/using-with-hooks
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+// NOTE: https://www.i18next.com/overview/configuration-options#configuration-options
 
-import Backend from 'i18next-http-backend';
+import i18next from 'i18next';
+import { initReactI18next as ReactI18NextInit } from 'react-i18next';
+
+import SyncBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-// not like to use this?
-// have a look at the Quick start guide 
-// for passing in lng and translations on init
 
-i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+i18next
+  .use(SyncBackend)
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
+  .use(ReactI18NextInit)
+  
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
     debug: true,
-
+    initImmediate: false,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     }
   });
 
 
-export default i18n;
+export default i18next;
