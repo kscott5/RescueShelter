1c64b9f82bbd1f20bfbb267353f6c25668e24ab0
Karega K. Scott <kkscott@outlook.com>
2025-04-15 16:51:28 -0500

    search stub

--- src/components/animals/listanimals.tsx
+++ src/components/animals/listanimals.tsx
@@ -2,13 +2,14 @@ import * as i18nextReact  from 'react-i18next';
 
 import * as React from 'react';
 import * as ReactRouterDom from 'react-router-dom';
+import { Checkbox, Select } from 'semantic-ui-react';
 
 function ListAnimals() {
     const localizer = i18nextReact.getI18n();
     const [model, setModel] = React.useState({ 
         data: [/*Array of HTML elements <div/> */], ok: true, 
         message: localizer.t(`components.http.get.loading`), 
-        options: {limit: 100, a11y: {lang: 'en'}}
+        options: {category_id: 5, keywords: '', limit: 100, endangered: false, a11y: {lang: 'en'}}
     });
 
     const loggedIn = false;
@@ -53,6 +54,56 @@ function ListAnimals() {
             <h2>{localizer.t('components.animals.headings.animal_list')}</h2>
             {(loggedIn)? (<ReactRouterDom.Link to="/animal">{localizer.t('components.links.new')}</ReactRouterDom.Link>) : <div/>}
             <div className={(model.ok)? "ui": "ui error"}>{model.message}</div>
+
+            <div>
+                <form id="search" name="search" className='ui form'>
+                    <div><h3>{localizer.t("component.search.options")}</h3></div>
+                    <div className="ui field">
+                        <label className="ui field label" htmlFor="categoryid" 
+                            aria-label={localizer.t("component.search.option.category")}>
+                                {localizer.t("component.search.option.category")}
+                        </label>
+                        <select id="categoryid" value={model.options.category_id} aria-label={localizer.t("component.search.option.category_id")}> 
+                            <option value="1">{localizer.t("component.search.option.invertebrates")}</option>
+                            <option value="2">{localizer.t("component.search.option.amphibians")}</option>
+                            <option value="3">{localizer.t("component.search.option.birds")}</option>
+                            <option value="4">{localizer.t("component.search.option.reptiles")}</option>
+                            <option value="5">{localizer.t("component.search.option.mammals")}</option>
+                            <option value="6">{localizer.t("component.search.option.fishes")}</option>
+                        </select>
+                    </div>
+                    <div className="ui field input">
+                        <label className="ui field label" htmlFor="endangered" 
+                            aria-label={localizer.t("component.search.option.endangered")}>
+                                {localizer.t("component.search.option.endangered")}
+                        </label>
+                        <Checkbox id="endangered" checked={model.options.endangered}>{localizer.t("component.search.option.endangered")}</Checkbox>
+                    </div>
+                    <div className="ui field input">
+                        <label className="ui field label" htmlFor="keywords" 
+                            aria-label={localizer.t("component.search.option.keywords")}>
+                                {localizer.t("component.search.option.keywords")}
+                        </label>
+                        <input id="keywords" type="text" value={model.options.keywords}></input>
+                    </div>
+                    <div>
+                        <label className="ui field label" htmlFor="limit" aria-label={localizer.t("component.search.option.limit")}>{localizer.t("component.search.option.limit")}</label>
+                        <select id="limit" value={model.options.limit}>
+                            <option value="25">25</option>
+                            <option value="50" >50</option>
+                            <option value="100">100</option>
+                            <option value="125">125</option>
+                            <option value="200">200</option>
+                        </select>
+                    </div>
+                    <div className="ui field input">
+                    <button type="button" className="ui button tiny circular" aria-label={localizer.t('components.buttons.login')}>
+                        {localizer.t('components.buttons.search')}
+                    </button>
+
+                    </div>
+                </form>
+            </div>
             {model.data}
         </div>
         
