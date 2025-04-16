64f871bd0fe2e6a94535a51c32d64a60e6cbded1
Karega K. Scott <kkscott@outlook.com>
2025-04-16 15:13:38 -0500

    animal search options.

--- src/components/animals/listanimals.tsx
+++ src/components/animals/listanimals.tsx
@@ -2,7 +2,6 @@ import * as i18nextReact  from 'react-i18next';
 
 import * as React from 'react';
 import * as ReactRouterDom from 'react-router-dom';
-import { Checkbox, Select } from 'semantic-ui-react';
 
 function ListAnimals() {
     const localizer = i18nextReact.getI18n();
@@ -15,39 +14,47 @@ function ListAnimals() {
     const loggedIn = false;
     const linkText = (loggedIn)? localizer.t('components.links.edit') : localizer.t('components.links.view');
     
-    React.useEffect(() => {
-        const httpGet = async() => {
-            setModel({...model, ok:true, message: localizer.t('component.http.get.loading')});
-            let response = await fetch(`/api/report/animals?limit=${model.options.limit}&lang=${model.options.a11y.lang}`);
-            
-            if(!response.ok) {                
-                console.error(response.statusText);
-                setModel({...model, ok: response.ok, message: localizer.t(`components.http.get.error`)});
-            } else {
-                let results = await response.json();
-                
-                let elements = [];
-                results.data.documents.map((element) =>
-                    elements.push( 
-                        <div key={element._id}>
-                            <span>{element.name}</span>
-                            <span>{element.description}</span>
-                            {
-                                (element.image?.contenttype == 'icon')?
-                                (<i className={element.image.content + ' ui massive ' + element.image.contenttype}/>) :
-                                ('&nbsp;')
-                            }
-                            <ReactRouterDom.Link to={`/animal/${element._id}`}>{linkText}</ReactRouterDom.Link>   
-                        </div>
-                    )
-                );
-
-                setModel({...model, ok: response.ok, message:'', data: elements});
+    const HandleSearch = React.useCallback(async () => {
+        setModel({...model, ok:true, message: localizer.t('component.http.get.loading')});
+        let response = await fetch(`/api/report/animals`,
+            {
+                method: 'POST',
+                body: JSON.stringify(model.options),
+                headers: {
+                    'content-type': 'application/json'
+                }
             }
-        };
+        );
+        
+        if(!response.ok) {                
+            console.error(response.statusText);
+            setModel({...model, ok: response.ok, message: localizer.t(`components.http.get.error`)});
+        } else {
+            let results = await response.json();
+            
+            let elements = [];
+            results.data.documents.map((element) =>
+                elements.push( 
+                    <div key={element._id}>
+                        <span>{element.name}</span>
+                        <span>{element.description}</span>
+                        {
+                            (element.image?.contenttype == 'icon')?
+                            (<i className={element.image.content + ' ui massive ' + element.image.contenttype}/>) :
+                            ('&nbsp;')
+                        }
+                        <ReactRouterDom.Link to={`/animal/${element._id}`}>{linkText}</ReactRouterDom.Link>   
+                    </div>
+                )
+            );
 
-        httpGet();
-    }, [ /* params */]); // end useEffect
+            setModel({...model, ok: response.ok, message:'', data: elements});
+        }
+    }, [ model ]); // end useCallback
+
+    React.useEffect(() => {
+        HandleSearch();
+    }, [/* dependency list */]); // end useEffect
 
     return (
         <div className="ui containter">
@@ -63,7 +70,10 @@ function ListAnimals() {
                             aria-label={localizer.t("component.search.option.category")}>
                                 {localizer.t("component.search.option.category")}
                         </label>
-                        <select id="categoryid" value={model.options.category_id} aria-label={localizer.t("component.search.option.category_id")}> 
+                        <select id="categoryid" defaultValue={model.options.category_id}
+                            aria-label={localizer.t("component.search.option.category_id")}
+                            onChange={(e) => setModel({...model, 
+                                options:{...model.options, category_id: parseInt(e.target.value)}})}>
                             <option value="1">{localizer.t("component.search.option.invertebrates")}</option>
                             <option value="2">{localizer.t("component.search.option.amphibians")}</option>
                             <option value="3">{localizer.t("component.search.option.birds")}</option>
@@ -77,18 +87,24 @@ function ListAnimals() {
                             aria-label={localizer.t("component.search.option.endangered")}>
                                 {localizer.t("component.search.option.endangered")}
                         </label>
-                        <Checkbox id="endangered" checked={model.options.endangered}>{localizer.t("component.search.option.endangered")}</Checkbox>
+                        <input id="endangered" type="checkbox" defaultChecked={model.options.endangered}
+                            onChange={(e) => setModel({...model, 
+                                options:{...model.options, endangered: e.target.checked}})}/>
                     </div>
                     <div className="ui field input">
                         <label className="ui field label" htmlFor="keywords" 
                             aria-label={localizer.t("component.search.option.keywords")}>
                                 {localizer.t("component.search.option.keywords")}
                         </label>
-                        <input id="keywords" type="text" value={model.options.keywords}></input>
+                        <input id="keywords" type="text" defaultValue={model.options.keywords}
+                            onChange={(e) => setModel({...model, 
+                                options:{...model.options, keywords: e.target.value}})}/>
                     </div>
                     <div>
                         <label className="ui field label" htmlFor="limit" aria-label={localizer.t("component.search.option.limit")}>{localizer.t("component.search.option.limit")}</label>
-                        <select id="limit" value={model.options.limit}>
+                        <select id="limit" defaultValue={model.options.limit} 
+                            onChange={(e) => setModel({...model, 
+                                options:{...model.options, limit: parseInt(e.target.value)}})}>
                             <option value="25">25</option>
                             <option value="50" >50</option>
                             <option value="100">100</option>
@@ -97,7 +113,9 @@ function ListAnimals() {
                         </select>
                     </div>
                     <div className="ui field input">
-                    <button type="button" className="ui button tiny circular" aria-label={localizer.t('components.buttons.login')}>
+                    <button type="button" className="ui button tiny circular" 
+                        onClick={HandleSearch}
+                        aria-label={localizer.t('components.buttons.login')}>
                         {localizer.t('components.buttons.search')}
                     </button>
 
